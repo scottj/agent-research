925957bea5235631824201a378a11dea5bb0302a
Scott Johnson <scottj@users.noreply.github.com>
2026-03-08 18:29:01 -0500

    build troubleshooting

--- .scripts/build-footer.sh
+++ .scripts/build-footer.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-mkdir _site/assets/js
-cp .scripts/footer.js _site/assets/js
+ls -la "${GITHUB_WORKSPACE}/_site"
+
+mkdir "${GITHUB_WORKSPACE}/_site/assets/js"
+cp .scripts/footer.js "${GITHUB_WORKSPACE}/_site/assets/js"
 
 FOOTER_HASH=$(git log -1 --format="%H" -- .scripts/footer.js)
 FOOTER_SHORT_HASH=$(echo "$FOOTER_HASH" | cut -c1-8)
 
-sed -i "s|</body>|<script type=\"module\" src=\"assets/js/footer.js?${FOOTER_SHORT_HASH}\"></script>\n</body>|" "_site/index.html"
+sed -i "s|</body>|<script type=\"module\" src=\"assets/js/footer.js?${FOOTER_SHORT_HASH}\"></script>\n</body>|" "${GITHUB_WORKSPACE}/_site/index.html"
